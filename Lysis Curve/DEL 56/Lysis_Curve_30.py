5815cee1722b4ac00845b9e9bf627a99790e9710
Naam's Desktop <noaml12@hotmail.com>
2021-08-31 12:07:20 +0300

    changed `figs:list` to `figs:dict`. figs saved in output folder is named after its own title

--- Lysis Curve/DEL 56/Lysis_Curve_30.py
+++ Lysis Curve/DEL 56/Lysis_Curve_30.py
@@ -18,16 +18,16 @@ df = analyse_file(
     opticalDense_path = f'./csv/{csv_filename}.csv',
     triplicates_path  = f'./groups/{groups_filename}.csv',
     blanks            = ['A3', 'A9'], # 2nd repeat
-    # blanks            = [], # 1st repeat
-    # parse_time        = False,
+    parse_time        = True,
 )
-figs = []
+figs = {}
 
 ## WT ##
 title = f'Lysis Curve - {deg}°C\nWT'
 cols = [ c for c in df.columns if 'WT' in c ]
 fig,axs = gen_pair_graphs(df, cols, title, xticks=range(time_in_oven+1))
-figs += [fig]
+title = title.split('\n')
+figs[title[1]] = fig
 ## WT ##
 
 wt_cols = [ c for c in df.columns if 'WT' in c and 'Del' not in c ]
@@ -37,22 +37,27 @@ wt_del_cols = [ c for c in df.columns if 'WT' in c and 'Del' in c ]
 title = f'Lysis Curve - {deg}°C\nΔStruc'
 cols = wt_cols + [ c for c in df.columns if 'Struc' in c ]
 fig,axs = gen_pair_graphs(df, cols, title, xticks=range(time_in_oven+1))
-figs += [fig]
+title = title.split('\n')
+figs[title[1]] = fig
 ## struc ##
 
 ## ΔΔholin-lysin ##
 title = f'Lysis Curve - {deg}°C\nΔΔholin-lysin'
 cols = wt_cols + [ c for c in df.columns if 'holin-lysin' in c ]
 fig,axs = gen_pair_graphs(df, cols, title, xticks=range(time_in_oven+1))
-figs += [fig]
+title = title.split('\n')
+figs[title[1]] = fig
 ## Δ55-6 ##
 
 
-# save
-for i,fig in enumerate(figs):
+# save dict
+for title,fig in figs.items():
     path = f'./figs/{output_folder}/{csv_filename}'
     os.makedirs(path, exist_ok=True)
-    fig.savefig(f'{path}/{i}.png')
+    print(f'saving {title} figure..')
+    fig.savefig(f'{path}/{title}.png')
+
+print('done')
 
 # show
 # plt.show()
