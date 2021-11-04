08ed89c227b4b9945c37554d85244e7cad8fff35
Naam's Desktop <noaml12@hotmail.com>
2021-11-04 12:42:00 +0200

    OE 55 touch-ups

--- Lysis Curve/OE 55/Lysis_Curve_30.py
+++ Lysis Curve/OE 55/Lysis_Curve_30.py
@@ -17,7 +17,6 @@ output_folder = 'lysis_curve_1_repeats'
 df = analyse_file(
     opticalDense_path = f'./csv/{csv_filename}.csv',
     triplicates_path  = f'./groups/{groups_filename}.csv',
-    # blanks            = ['A3'] # for deg30-rept1
     blanks            = ['A3', 'A9'] # for all else
 )
 figs = []
@@ -30,6 +29,7 @@ figs += [fig]
 ## WT ##
 
 wt_cols = [ c for c in df.columns if 'WT' in c and 'OE' not in c ]
+wt_cols_minus_at = [ c for c in df.columns if 'WT' in c and 'OE' not in c and '-AT' in c ]
 wt_oe_cols = [ c for c in df.columns if 'WT' in c and 'OE' in c ]
 
 ## Cured  ##
@@ -53,6 +53,13 @@ fig,axs = gen_pair_graphs(df, cols, title, xticks=range(time_in_oven+1))
 figs += [fig]
 ## Δ55-6 ##
 
+## Δ1555-56 ##
+title = f'Lysis Curve - {deg}°C\nΔ1555-56'
+cols = wt_cols_minus_at + [ c for c in df.columns if '55-6' in c and 'OE' not in c and '-AT' in c ]
+fig,axs = gen_pair_graphs(df, cols, title, xticks=range(time_in_oven+1))
+figs += [fig]
+## Δ1555-56 ##
+
 
 # save
 for i,fig in enumerate(figs):
@@ -61,4 +68,4 @@ for i,fig in enumerate(figs):
     fig.savefig(f'{path}/{i}.png')
 
 # show
-# plt.show()
+# plt.show() 
